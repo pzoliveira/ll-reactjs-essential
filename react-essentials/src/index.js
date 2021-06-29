c4bfd445d36190ffeb334df92189312a16a24a0f
pzoliveira <pzoliveira@outlook.com>
2021-06-29 15:37:56 -0300

    Aula 02 p2

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -5,10 +5,11 @@ import './index.css';
 ReactDOM.render(
   React.createElement(
     "ul",
-    {style: {color: "blue"}},
+    null,
     React.createElement("li", null, "Monday"),
     React.createElement("li", null, "Tuesday"),
     React.createElement("li", null, "Wednesday"),
-    React.createElement("li", null, "Thursday"),
+    React.createElement("li", null, "Thursday")
+  ),
   document.getElementById('root')
 );
