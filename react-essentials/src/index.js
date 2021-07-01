ac980a578fbd3be281d907eba117ae263a0b2443
pzoliveira <pzoliveira@outlook.com>
2021-06-30 21:00:22 -0300

    Aula 03 p3

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+function AppTwo(){
+  return(
+    <h1>
+        This is a second application.
+    </h1>
+  )
+}
+
 ReactDOM.render(
+  <>
     <App />
-    ,
+    <AppTwo />
+  </>,
   document.getElementById('root')
 );
