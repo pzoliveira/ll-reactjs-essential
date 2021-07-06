ed838a5303173fc174a78d107c3f5e5cdb94034c
pzoliveira <pzoliveira@outlook.com>
2021-07-06 10:21:49 -0300

    Aula 04 - p1

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,61 +1,21 @@
+import React from "react";
 import './App.css';
-import restaurant from './restaurant.jpg';
 
-let dishes = [
-  "Maccaroni and cheese",
-  "Salmon",
-  "Tofu with vegetables",
-  "Minestrone"
-];
-
-let dishObjects = dishes.map((dish, i) => ({id: i, title: dish}));
-
-function Header(props){
-  return(
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  );
+function SecretComponent() {
+  return <h1>Secret information for authorized users only</h1>
 }
 
-function Main(props){
-  return(
-    <section>
-      <p>
-        We serve the most {props.adjective} food around.
-      </p>
-      <p>
-        <img
-          src={restaurant}
-          height={200}
-          alt="Tables and chairs of a luxuous restaurant in London."
-        />
-      </p>
-        <ul style={{textAlign: "left"}}>
-          {props.dishes.map((dish) =>(
-          <li key={dish.id}>{dish.title}</li>
-          ))}
-        </ul>
-    </section>
-  );
+function RegularComponent() {
+  return <h1>Everyone can see this</h1>
 }
 
-function Footer(props){
-  return(
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
-}
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Camila" />
-      <Main adjective="amazing" dishes={dishObjects} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+function App(props) {
+  return props.authorized ? <SecretComponent /> : <RegularComponent />;
+  /*if (props.authorized){
+    return <SecretComponent />
+  } else {
+    return <RegularComponent />
+  }*/
 }
 
 export default App;
