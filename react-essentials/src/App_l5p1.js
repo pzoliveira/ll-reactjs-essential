effc0a5c65217a8336e7dcc36625285c1b34f063
pzoliveira <pzoliveira@outlook.com>
2021-07-06 12:40:31 -0300

    Aula 05 p3

---
+++ react-essentials/src/App_l5p1.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+
+// https://api.github.com/users/pzoliveira
+
+function App({login}) {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+  
+  if (data) {
+    return <div>{JSON.stringify(data)}</div>;
+  }
+
+  return <div>No user available</div>
+}
+
+export default App;
---
+++ react-essentials/src/App_l5p2.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+
+// https://api.github.com/users/pzoliveira
+
+function App({login}) {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+  
+  if (data) {
+    return (<div>
+      <h1>{data.name}</h1>
+      <p>{data.location}</p>
+      <img alt={data.login} src={data.avatar_url} />
+      </div>);
+  }
+
+  return <div>No user available</div>
+}
+
+export default App;
