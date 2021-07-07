f23007ea00c038f621493ff6aa69c20256499902
pzoliveira <pzoliveira@outlook.com>
2021-07-07 10:33:57 -0300

    Aula 07

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,11 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './App.css';
+import { Routes, Route} from "react-router-dom";
+import { Home, About, Services, CompanyHistory, Location, Events, Contact, Whoops404} from "./pages";
 
 function App() {
 
-  return (<div>
-    <h1>Hello React Testing Library</h1>
-    </div>);
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="services" element={<Services />} />
+          <Route path="history" element={<CompanyHistory />} />
+          <Route path="location" element={<Location />} />
+        </Route>
+        <Route path="/events" element={<Events />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Whoops404 />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
